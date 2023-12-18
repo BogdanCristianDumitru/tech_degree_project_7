9cea22be2ed2d9a21b5761c087041bcc1893ae53
Bogdan <bogdan_dumitru_85@yahoo.com>
2023-12-18 09:16:53 -0500

    js and alert-banner added

---
+++ script.js
@@ -0,0 +1,10 @@
+const alertBanner = document.getElementById("alert");
+
+
+
+alertBanner.innerHTML = `
+    <div class="alert-banner">
+        <p><strong>Alert:</strong> You have unread messages</p>
+        <p class="alert-banner-close">x</p>
+    </div>
+`;
