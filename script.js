1cd6b54f6f26605427b84fd089b4c09d6a481c88
Bogdan <bogdan_dumitru_85@yahoo.com>
2023-12-19 11:31:05 -0500

    charts added

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const alertBanner = document.getElementById("alert");
 
+const trafficCanvas = document.getElementById("traffic_chart");
+const dailyCanvas = document.getElementById('daily_chart');
+const mobileCanvas = document.getElementById('doghnut_chart');
 
 
 alertBanner.innerHTML = `
@@ -8,3 +11,126 @@ alertBanner.innerHTML = `
         <p class="alert-banner-close">x</p>
     </div>
 `;
+
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = "none";
+    }
+});
+
+/* Object literal represeting the data for the line chart */
+let trafficData = {
+    labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+    datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+            2500],
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderWidth: 1,    
+    }]
+};
+
+/* Object literal with the options that change for the line chart */
+let trafficOptions = {
+    backgroundColor: 'rgba(112, 104, 201, .5)',
+    fill: true,
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0
+        },
+    scales: {
+        y: {
+        beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+        display: false
+        }
+    }
+};
+
+/* Line graph chart */
+
+trafficOptions.responsive = true;
+trafficOptions.maintainAspectRatio = false;
+
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions,
+});
+
+/* Object literal represeting the data for the bar chart */
+const dailyData = {
+    labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1 
+    }]
+};
+
+/* Object literal with options for the bar chart */
+const dailyOptions = {
+    scales: {
+        y: {
+        beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+        display: false
+        }
+    }
+};
+
+/* Bar graph chart */
+dailyOptions.responsive = true;
+dailyOptions.maintainAspectRatio = false;
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+/* Object literal represeting the data for the doughnut chart */
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8'
+            ]
+        }]
+    };
+
+    /* Object literal with optionsfor the doughnut chart */
+    const mobileOptions = {
+        aspectRatio: 1.9,
+        plugins: {
+            legend: {
+                position: 'right',
+                labels: {
+                    boxWidth: 20,
+                    fontStyle: 'bold'
+                 }
+             }
+        }
+    };
+
+    /* Doughnut Chart */
+    mobileOptions.responsive = true;
+    mobileOptions.maintainAspectRatio = false;
+
+    let mobileChart = new Chart(mobileCanvas, {
+        type: 'doughnut',
+        data: mobileData,
+        options: mobileOptions
+    });
